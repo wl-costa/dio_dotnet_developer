3584dc7997481e2b1a8e5384247aba8b7d0b4a13
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-11 20:51:42 -0300

    altera propriedades da classe através da variável e chama o método Apresentar() para exibir as alterações com base nessa variável

--- PessoaClass/Models/Pessoa.cs
+++ PessoaClass/Models/Pessoa.cs
@@ -7,10 +7,10 @@ namespace PessoaClass.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+        public string Nome { get; set; } // propriedade da classe
+        public int Idade { get; set; } // propriedade da classe
 
-        public void Apresentar()
+        public void Apresentar() // um método, a.k.a function (javascript)
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
         }
--- PessoaClass/Program.cs
+++ PessoaClass/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using PessoaClass.Models; // solicita a classe correta através do namespace
+
+Pessoa pessoa1 = new Pessoa(); // pessoa1 é a variável
+
+pessoa1.Nome = "Wendell"; // altera a propriedade Nome
+pessoa1.Idade = 22; // altera a propriedade Idade
+pessoa1.Apresentar(); // executa o método / função
