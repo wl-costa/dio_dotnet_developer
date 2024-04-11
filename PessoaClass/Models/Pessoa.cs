fbb33bea88ef2d27eaa9eddcd9a2d87502b719b4
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-11 19:47:08 -0300

    *

--- PessoaClass/Models/Pessoa.cs
+++ PessoaClass/Models/Pessoa.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace pessoa_class.Models
+namespace PessoaClass.Models
 {
     public class Pessoa
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
-        
-          public void Apresentar()
+
+        public void Apresentar()
         {
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
         }
     }
 }
