2d28c89fdef52caca2eb4e388abbe27ffe7aef30
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-11 18:59:51 -0300

    created 'models' folder and Pessoa.cs

---
+++ pessoa_class/models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pessoa_class.models
+{
+    public class Pessoa
+    {
+        
+    }
+}
