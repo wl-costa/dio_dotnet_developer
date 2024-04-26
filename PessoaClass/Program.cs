ba3b984c99303523e4ad4dfa41317c0ef010c031
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-26 13:34:10 -0300

    criação de uma variável independente de classe

--- PessoaClass/Program.cs
+++ PessoaClass/Program.cs
@@ -1,18 +1,21 @@
 ﻿using PessoaClass.Models; // solicita a classe correta através do namespace
 
-Pessoa pessoa1 = new() // nova variável
+Pessoa pessoa1 = new() // nova variável com as props da classe
 {
     Nome = "Wendell", // altera a propriedade Nome
     Idade = 22, // altera a propriedade Idade
     Dinheiro = 1286.27M
 };
 
-Pessoa pessoa2 = new() // nova variável
+Pessoa pessoa2 = new() // nova variável com as props da classe
 {
     Nome = "Thomas", // altera a propriedade Nome
     Idade = 18, // altera a propriedade Idade
     Dinheiro = 816.19M
 };
 
+decimal dinheiroSomado = pessoa1.Dinheiro + pessoa2.Dinheiro;
+
 pessoa1.Apresentar(); // executa o método / função
 pessoa2.Apresentar(); // executa o método / função
+Console.WriteLine($"Juntos vocês tem o total de R${dinheiroSomado}");
