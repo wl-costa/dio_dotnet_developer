b2bc98e3c1c10e8bd45c0030110df8aabca72c86
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-26 13:18:31 -0300

    faz uso do tipo de dados decimal para representar dinheiro

--- PessoaClass/Models/Pessoa.cs
+++ PessoaClass/Models/Pessoa.cs
@@ -9,12 +9,13 @@ namespace PessoaClass.Models
     {
         public string Nome { get; set; } // propriedade da classe
         public int Idade { get; set; } // propriedade da classe
+        public decimal Dinheiro { get; set; }
 
         public void Apresentar() // um método, a.k.a function (javascript)
         {
             if (Idade >= 18) // checa se a idade é maior ou igual a 18
             {
-                Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos.");
+                Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. Possuo R${Dinheiro} em conta");
             } else 
             {
                 Console.WriteLine($"Desculpe {Nome}, você precisa ser maior de 18 anos para rodar esse programa.");
--- PessoaClass/Program.cs
+++ PessoaClass/Program.cs
@@ -3,13 +3,15 @@
 Pessoa pessoa1 = new() // nova variável
 {
     Nome = "Wendell", // altera a propriedade Nome
-    Idade = 22 // altera a propriedade Idade
+    Idade = 22, // altera a propriedade Idade
+    Dinheiro = 1286.27M
 };
 
 Pessoa pessoa2 = new() // nova variável
 {
     Nome = "Thomas", // altera a propriedade Nome
-    Idade = 17 // altera a propriedade Idade
+    Idade = 18, // altera a propriedade Idade
+    Dinheiro = 816.19M
 };
 
 pessoa1.Apresentar(); // executa o método / função
