65676f02e69eb40501e234ed92b90b9c094f756e
Wendell Costa <wendellcosta.dev@gmail.com>
2024-04-30 13:23:29 -0300

    faz com que o programa não rode caso as duas pessoas não possuam pelo menos 18 anos/Agora o programa é capaz de rodar individualmente cada pessoa baseado em sua idade

--- PessoaClass/Models/Pessoa.cs
+++ PessoaClass/Models/Pessoa.cs
@@ -13,14 +13,12 @@ namespace PessoaClass.Models
 
         public void Apresentar() // um método, a.k.a function (javascript)
         {
-            if (Idade >= 18) // checa se a idade é maior ou igual a 18
-            {
-                Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos. Possuo R${Dinheiro} em conta\n");
-            }
-            else
-            {
-                Console.WriteLine($"Desculpe {Nome}, você precisa ser maior de 18 anos para rodar esse programa.\n");
-            }
+            Console.WriteLine($"Olá {Nome}. Você possui R${Dinheiro} em conta\n");
+        }
+
+        public static void MensagemDeErroIdade()
+        {  
+            Console.WriteLine($"Desculpe, você precisa ser maior de 18 anos para rodar esse programa.\n");
         }
 
         public static void ShowDateTime()
@@ -31,22 +29,39 @@ namespace PessoaClass.Models
             Console.WriteLine($"\nAplicação executada no dia {data} às {horario}\n");
         }
 
-        public static decimal DinheiroTotal(decimal Pessoa1, decimal Pessoa2)
+        public static decimal DinheiroTotalCasal(decimal pessoa1, decimal pessoa2)
         {
-            decimal dinheiroTotal = Pessoa1 + Pessoa2;
+            decimal dinheiroTotal = pessoa1 + pessoa2;
             Console.WriteLine($"Juntos vocês tem o total de R${dinheiroTotal}\n");
             /*int dinheiroTotalInt = Convert.ToInt32(dinheiroTotal); uma forma de converter decimal para int*/
+            /*int dinheiroTotalInt = int.Parse(dinheiroTotal); outra forma de converter decimal para int*/
             return dinheiroTotal;
         }
 
-        public static void DinheiroTotalMenosDivida(decimal dinheiro)
+        public static decimal DinheiroTotal(decimal pessoa)
+        {
+            decimal dinheiroTotal = pessoa;
+            return pessoa;
+        }
+
+        public static void DinheiroTotalMenosDividaCasal(decimal dinheiro)
         {
             decimal divida = 500.38M;
-            DateTime dataLimite = DateTime.Today.AddDays(28); 
+            DateTime dataLimite = DateTime.Today.AddDays(28);
             string dataString = dataLimite.ToString("dd/MM/yyyy");
             Console.WriteLine($"Vocês possuem uma dívida de R${divida}\n");
             Console.WriteLine($"Vocês tem até o dia {dataString} para pagar suas dívidas sem cobrança de juros\n");
             Console.WriteLine($"Subtraindo as dívidas, vocês possuem R${dinheiro - divida}\n");
+        }
+
+        public static void DinheiroTotalMenosDivida(decimal dinheiro)
+        {
+            decimal divida = 250.19M;
+            DateTime dataLimite = DateTime.Today.AddDays(28);
+            string dataString = dataLimite.ToString("dd/MM/yyyy");
+            Console.WriteLine($"Você possui uma dívida de R${divida}\n");
+            Console.WriteLine($"Você tem até o dia {dataString} para pagar a dívida sem cobrança de juros\n");
+            Console.WriteLine($"Subtraindo a dívida, você possui R${dinheiro - divida}\n");
         }
     }
 }
--- PessoaClass/Program.cs
+++ PessoaClass/Program.cs
@@ -3,18 +3,37 @@
 Pessoa pessoa1 = new() // nova variável com as props da classe
 {
     Nome = "Wendell", // altera a propriedade Nome
-    Idade = 22, // altera a propriedade Idade
+    Idade = 18, // altera a propriedade Idade
     Dinheiro = 1286.27M
 };
 
 Pessoa pessoa2 = new() // nova variável com as props da classe
 {
     Nome = "Thomas", // altera a propriedade Nome
-    Idade = 20, // altera a propriedade Idade
+    Idade = 18, // altera a propriedade Idade
     Dinheiro = 816.19M
 };
 
-Pessoa.ShowDateTime();
-pessoa1.Apresentar(); // executa o método / função
-pessoa2.Apresentar(); // executa o método / função
-Pessoa.DinheiroTotalMenosDivida(Pessoa.DinheiroTotal(pessoa1.Dinheiro, pessoa2.Dinheiro));
+if (pessoa1.Idade >= 18 && pessoa2.Idade >= 18)
+{
+    Pessoa.ShowDateTime();
+    pessoa1.Apresentar();
+    pessoa2.Apresentar();
+    Pessoa.DinheiroTotalMenosDividaCasal(Pessoa.DinheiroTotalCasal(pessoa1.Dinheiro, pessoa2.Dinheiro));
+}
+else if (pessoa1.Idade >= 18)
+{
+    Pessoa.ShowDateTime();
+    pessoa1.Apresentar();
+    Pessoa.DinheiroTotalMenosDivida(Pessoa.DinheiroTotal(pessoa1.Dinheiro));
+}
+else if (pessoa2.Idade >= 18)
+{
+    Pessoa.ShowDateTime();
+    pessoa2.Apresentar();
+    Pessoa.DinheiroTotalMenosDivida(Pessoa.DinheiroTotal(pessoa2.Dinheiro));
+} else
+{
+    Pessoa.MensagemDeErroIdade();
+}
+
